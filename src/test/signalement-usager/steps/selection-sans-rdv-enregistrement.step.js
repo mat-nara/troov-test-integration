efb9e2af29f2610ebe9d533d4452ae141709bc7f
Mahefa <mahefa@troov.com>
2025-02-21 17:34:29 +0300

    Update to incorporate world.

--- src/test/signalement-usager/steps/selection-sans-rdv-enregistrement.step.js
+++ src/test/signalement-usager/steps/selection-sans-rdv-enregistrement.step.js
@@ -1,103 +1,91 @@
-const { Given, When, Then, BeforeAll, AfterAll, setDefaultTimeout } = require('@cucumber/cucumber');
-const { chromium, expect } = require('@playwright/test');
-const { initializeBrowser, closeBrowser } = require('./browserSetup');
+const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
 const config = require('../../../../config/env.js')
 
 
 setDefaultTimeout(60 * 1000);
 
-let browser, context, page;
-
-BeforeAll(async function () {
-  ({ browser, context, page } = await initializeBrowser());
-});
-
-Given("La page \"Je m'enregistre\" sans rendez-vous est ouverte", async () => {
-  await page.goto(config.troovCafUserArrivalURL);
-  const sansRdvButton = page.locator('button[name="with-rdv"]');
+Given("La page \"Je m'enregistre\" sans rendez-vous est ouverte", async function() {
+  await this.terminalPage.goto(config.troovCafUserArrivalURL);
+  const sansRdvButton = this.terminalPage.locator('button[name="with-rdv"]');
   await sansRdvButton.waitFor();
   await sansRdvButton.click();
-  const heading = await page.getByText("Je m'enregistre");
+  const heading = await this.terminalPage.getByText("Je m'enregistre");
   await expect(heading).toBeVisible();
 });
 
 
 // Scenario NIR uniquement
-Given("NIR uniquement est saisie", async () => {
-  const inputNIRLocator = page.locator('label[for="social-security-number"] + input');
+Given("NIR uniquement est saisie", async function() {
+  const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
   await inputNIRLocator.fill('1234567891234');
-  const buttonLocator = await page.locator('button[aria-label="Continuer"]');
+  const buttonLocator = await this.terminalPage.locator('button[aria-label="Continuer"]');
   await expect(buttonLocator).toBeEnabled();
 });
 
 // Scenario Téléphone uniquement
-Given("Téléphone uniquement est saisie", async () => {
-  const inputPhoneLocator = page.locator('label[for="phone-number"] + input');
+Given("Téléphone uniquement est saisie", async function() {
+  const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
   await inputPhoneLocator.fill('1234567891');
-  const buttonLocator = await page.locator('button[aria-label="Continuer"]');
+  const buttonLocator = await this.terminalPage.locator('button[aria-label="Continuer"]');
   await expect(buttonLocator).toBeEnabled();
 });
 
 // Scenario NIR et Téléphone sont saisie
-Given("NIR et Téléphone sont saisie", async () => {
-  const inputNIRLocator = page.locator('label[for="social-security-number"] + input');
+Given("NIR et Téléphone sont saisie", async function() {
+  const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
   await inputNIRLocator.fill('1234567891234');
-  const inputPhoneLocator = page.locator('label[for="phone-number"] + input');
+  const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
   await inputPhoneLocator.fill('1234567891');
-  const buttonLocator = await page.locator('button[aria-label="Continuer"]');
+  const buttonLocator = await this.terminalPage.locator('button[aria-label="Continuer"]');
   await expect(buttonLocator).toBeEnabled();
 });
 
 // Scenario NIR saisie au mauvais format
-Given("que le NIR est saisi au mauvais format dans un signalement sans rendez-vous", async () => {
-  const inputNIRLocator = page.locator('label[for="social-security-number"] + input');
+Given("que le NIR est saisi au mauvais format dans un signalement sans rendez-vous", async function() {
+  const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
   await inputNIRLocator.fill('123456789111'); // 12 caracter au lieu de 13
 
-  const inputPhoneLocator = page.locator('label[for="phone-number"] + input');
+  const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
   await inputPhoneLocator.fill('1234567891');
 
-  const buttonLocator = await page.locator('button[aria-label="Continuer"]');
+  const buttonLocator = await this.terminalPage.locator('button[aria-label="Continuer"]');
   await expect(buttonLocator).toBeEnabled();
 });
 
 // Scenario Téléphone saisie au mauvais format
-Given("que le téléphone est saisi au mauvais format dans un signalement sans rendez-vous", async () => {
-  const inputNIRLocator = page.locator('label[for="social-security-number"] + input');
+Given("que le téléphone est saisi au mauvais format dans un signalement sans rendez-vous", async function() {
+  const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
   await inputNIRLocator.fill('1234567891111'); 
 
-  const inputPhoneLocator = page.locator('label[for="phone-number"] + input');
+  const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
   await inputPhoneLocator.fill('123456789'); // 9 caracter au lieu de 10
 
-  const buttonLocator = await page.locator('button[aria-label="Continuer"]');
+  const buttonLocator = await this.terminalPage.locator('button[aria-label="Continuer"]');
   await expect(buttonLocator).toBeEnabled();
 });
 // ------------------------------------------------------------------------
 
-When("Cliquer sur le bouton 'Continuer' de la page d'enregistrement", async () => {
-  await page.locator('button[aria-label="Continuer"]').click();
+When("Cliquer sur le bouton 'Continuer' de la page d'enregistrement", async function() {
+  await this.terminalPage.locator('button[aria-label="Continuer"]').click();
 });
 
-When("Cliquer sur 'Quitter' de la page d'enregistrement", async () => {
-  await page.locator('button[aria-label="Quitter"]').click();
+When("Cliquer sur 'Quitter' de la page d'enregistrement", async function() {
+  await this.terminalPage.locator('button[aria-label="Quitter"]').click();
 });
 // ------------------------------------------------------------------------
 
-Then("Passe à l'etape suivant: \"Je choisis mon motif de visite\" s'affiche sur la page", async () => {
-  const heading = await page.getByText("Je choisis mon motif de visite");
+Then("Passe à l'etape suivant: \"Je choisis mon motif de visite\" s'affiche sur la page", async function() {
+  const heading = await this.terminalPage.getByText("Je choisis mon motif de visite");
   await expect(heading).toBeVisible();
 });
 
-Then("Message d'erreur s'affiche", async () => {
-  const heading = await page.getByText("Saisie incorrecte");
+Then("Message d'erreur s'affiche", async function() {
+  const heading = await this.terminalPage.getByText("Saisie incorrecte");
   await expect(heading).toBeVisible();
 });
 
-Then("Revient sur la page initiale depuis la page d'enregistrement, signalement sans rendez-vous: \"Je signale mon arrivée\" s'affiche sur la page", async () => {
-  const heading = await page.getByText("Je signale mon arrivée");
+Then("Revient sur la page initiale depuis la page d'enregistrement, signalement sans rendez-vous: \"Je signale mon arrivée\" s'affiche sur la page", async function() {
+  const heading = await this.terminalPage.getByText("Je signale mon arrivée");
   await expect(heading).toBeVisible();
 });
-
-
-AfterAll(async function () {
-  await closeBrowser();
-})
