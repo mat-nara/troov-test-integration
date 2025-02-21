aa766d552ef95a799d11d0ad6bf448bc0920067b
Mahefa <mahefa@troov.com>
2025-02-21 16:40:07 +0300

    Adapt TTI-13 to use world

--- src/test/signalement-usager/steps/selection-sans-rdv-confirmation.step.js
+++ src/test/signalement-usager/steps/selection-sans-rdv-confirmation.step.js
@@ -1,64 +1,57 @@
-const { Given, When, Then, BeforeAll, AfterAll, setDefaultTimeout } = require('@cucumber/cucumber');
+const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
-const { initializeBrowser, closeBrowser } = require('./browserSetup');
 const config = require('../../../../config/env.js')
 
 
 setDefaultTimeout(60 * 1000);
 
-let browser, context, page;
-
-BeforeAll(async function () {
-  ({ browser, context, page } = await initializeBrowser());
-});
-
-Given("La page de confirmation de la création de ticket sans rendez-vous est ouverte", async () => {
+Given("La page de confirmation de la création de ticket sans rendez-vous est ouverte", async function() {
 
   // Page principale
-  await page.goto(config.troovCafUserArrivalURL);
-  const sansRdvButton = page.locator('button[name="with-rdv"]');
+  await this.terminalPage.goto(config.troovCafUserArrivalURL);
+  const sansRdvButton = this.terminalPage.locator('button[name="with-rdv"]');
   await sansRdvButton.waitFor();
   await sansRdvButton.click();
 
   // Page enregistrement
-  const heading = await page.getByText("Je m'enregistre");
+  const heading = await this.terminalPage.getByText("Je m'enregistre");
   await expect(heading).toBeVisible();
-  const inputNIRLocator = page.locator('label[for="social-security-number"] + input');
+  const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
   await inputNIRLocator.fill('1234567891111');
-  const inputPhoneLocator = page.locator('label[for="phone-number"] + input');
+  const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
   await inputPhoneLocator.fill('1234567891');
-  const buttonLocator = await page.locator('button[aria-label="Continuer"]');
+  const buttonLocator = await this.terminalPage.locator('button[aria-label="Continuer"]');
   await expect(buttonLocator).toBeEnabled();
   await buttonLocator.click();
 
   // Page Motif de visite 
-  const headingMotif = await page.getByText("Je choisis mon motif de visite");
+  const headingMotif = await this.terminalPage.getByText("Je choisis mon motif de visite");
   await expect(headingMotif).toBeVisible();
-  const inputOfficeTypeLocator = page.locator('label[for="office-type"] + div span.p-select-label');
+  const inputOfficeTypeLocator = this.terminalPage.locator('label[for="office-type"] + div span.p-select-label');
   await inputOfficeTypeLocator.click();
-  const liEnfantLocator = page.locator(`li[aria-label="Enfant"]`);
+  const liEnfantLocator = this.terminalPage.locator(`li[aria-label="Enfant"]`);
   await liEnfantLocator.click();
   const updatedTextOfficeType = await inputOfficeTypeLocator.textContent();
   expect(updatedTextOfficeType.trim()).toBe("Enfant");
-  const inputOfficeLocator = page.locator('label[for="office"] + div span.p-select-label');
+  const inputOfficeLocator = this.terminalPage.locator('label[for="office"] + div span.p-select-label');
   await inputOfficeLocator.click();
-  const liAttendEnfantLocator = await page.locator(`li[aria-label="J'attends / J'accueille un enfant"]`);
+  const liAttendEnfantLocator = await this.terminalPage.locator(`li[aria-label="J'attends / J'accueille un enfant"]`);
   await liAttendEnfantLocator.click();
   const updatedTextOffice = await liAttendEnfantLocator.textContent();
   expect(updatedTextOffice.trim()).toBe("J'attends / J'accueille un enfant");
-  await page.locator('button[aria-label="Continuer"]').click();
+  await this.terminalPage.locator('button[aria-label="Continuer"]').click();
 
   // Page de confirmation
 });
 
 // Téléchargement du ticket digital
-When("L'utilisateur clique sur le bouton \"Télécharger mon ticket\" de la page confirmation signalement sans rendez-vous", async () => {
-  await page.getByText('Télécharger mon ticket').click();
+When("L'utilisateur clique sur le bouton \"Télécharger mon ticket\" de la page confirmation signalement sans rendez-vous", async function() {
+  await this.terminalPage.getByText('Télécharger mon ticket').click();
 });
 
 // La page de confirmation sans rendez-vous s'affiche correctement 
-Then("Le titre \"Vous êtes bien enregistré !\" s'affiche sur la page de confirmation signalement sans rendez-vous", async () => {
-  const heading = await page.getByText("Vous êtes bien enregistré !");
+Then("Le titre \"Vous êtes bien enregistré !\" s'affiche sur la page de confirmation signalement sans rendez-vous", async function() {
+  const heading = await this.terminalPage.getByText("Vous êtes bien enregistré !");
   await expect(heading).toBeVisible();
 });
 
@@ -85,7 +78,7 @@ Then("Tous les informations sur le signalement sans rendez-vous s'affiche correc
 		expect(motif).toBeTruthy();
 });
 
-Then("Le fichier ticket digital sur page de confirmation sans rendez-vous doit être téléchargé", async () => {
+Then("Le fichier ticket digital sur page de confirmation sans rendez-vous doit être téléchargé", async function() {
   const download = await this.terminalPage.waitForEvent('download');
 	expect(download.suggestedFilename()).toBe("ticket.pdf");
 	const filePath = await download.path();
@@ -93,6 +86,3 @@ Then("Le fichier ticket digital sur page de confirmation sans rendez-vous doit �
 	await download.delete();
 });
 
-AfterAll(async function () {
-  await closeBrowser();
-})
