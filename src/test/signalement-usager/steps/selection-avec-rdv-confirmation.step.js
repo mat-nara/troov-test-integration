05bf46cf6dec1d1a8580b25a67bd69c5c4ed0933
Mahefa <mahefa@troov.com>
2025-08-28 11:21:44 +0300

    Update: add each information in confirmation page checking

--- src/test/signalement-usager/steps/selection-avec-rdv-confirmation.step.js
+++ src/test/signalement-usager/steps/selection-avec-rdv-confirmation.step.js
@@ -1,11 +1,185 @@
-const { Given, When, Then, BeforeAll, AfterAll, setDefaultTimeout } = require('@cucumber/cucumber');
+const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
-const { initializeBrowser, closeBrowser } = require('./browserSetup');
+const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');
 const config = require('../../../../config/env.js')
-const fs = require('fs');
+const { generateRandomNIR, generateRandomPhone } = require('../utils/helper');
+const LoginPage = require('../pages/LoginPage');
+const { faker, fakerFR  } = require('@faker-js/faker');
 
 
 setDefaultTimeout(60 * 1000);
+
+function randomNIR() {
+  const sexe = Math.random() < 0.5 ? 1 : 2;          // 1 ou 2
+  const annee = String(Math.floor(Math.random() * 100)).padStart(2, '0'); // 00..99
+  const mois = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0'); // 01..12
+  const dept = String(Math.floor(Math.random() * 96) + 1).padStart(2, '0'); // 01..95
+  const commune = String(Math.floor(Math.random() * 990) + 1).padStart(3, '0');
+  const ordre = String(Math.floor(Math.random() * 990) + 1).padStart(3, '0');
+
+  return `${sexe} ${annee} ${mois} ${dept} ${commune} ${ordre}`;
+}
+
+
+async function setupRdv(world) {
+
+    console.log('==> CREATION OF RDV');
+    await world.backofficePage.waitForTimeout(2000);
+
+    // Open window Ajouter un RDV
+    await world.backofficePage.locator('button[title="Ajouter un RDV"]').click();
+
+    // Choix du Service (1st item)
+    world.motif = "06 - J'ai une demande concernant le handicap ou la dépendance - Je souhaite faire une demande d'allocation journalière de proche aidant (AJPA)"
+    const selectorService = world.backofficePage.locator('span').filter({ hasText: 'Choisir un service' })
+    await selectorService.click();
+    const selectorServiceLegend = world.backofficePage.locator('legend').filter({ hasText: 'Service' })
+    const firstItemService = selectorServiceLegend.locator('xpath=following-sibling::*').locator('div.multiselect__content-wrapper > ul.multiselect__content > li').filter({ hasText: world.motif }).first(); 
+
+    await firstItemService.click();
+
+    // Choix du mode
+    // const selectorModeDuRDV = world.backofficePage.locator('label').filter({ hasText: 'Modes de RDV' }).locator('xpath=following-sibling::*');
+    // await selectorModeDuRDV.selectOption({ index: 0 });  
+
+    // Select heure du RDV 
+    // const selectorDateDuRDV = world.backofficePage.locator('label').filter({ hasText: 'Date du RDV' }).locator('xpath=following-sibling::span//div//input');
+    // await selectorDateDuRDV.click();
+    // world.backofficePage.locator('span[aria-label="jeudi 28 août 2025"]').nth(1).click();
+    // await selectorDateDuRDV.fill('28/08/2025');
+
+    const selectorHeureDuRDV = world.backofficePage.locator('legend').filter({ hasText: 'Heure du RDV' }).locator('xpath=following-sibling::div//select');
+    await selectorHeureDuRDV.selectOption({ index: 1 });  //
+
+    // Create new user
+    const selectorButtonCreerUser = world.backofficePage.locator('button > span').filter({ hasText: 'Créer un utilisateur' });
+    await selectorButtonCreerUser.click();
+
+    // Generate random data for appointment
+    world.name = faker.person.lastName();
+    world.firstname = faker.person.firstName();
+    world.email = world.name.toLowerCase() + '@test.com';
+    // world.NIR = generateRandomNIR();
+    world.NIR = randomNIR()
+    world.phone = generateRandomPhone();
+
+    await world.backofficePage.getByPlaceholder('Ajouter un Nom').fill(world.name);
+    await world.backofficePage.getByPlaceholder('Ajouter un Prénom').fill(world.firstname);
+    await world.backofficePage.getByPlaceholder('Ajouter un Email').fill(world.email);
+    await world.backofficePage.getByPlaceholder('1 48 05 99 *** ***').fill(world.NIR);
+    await world.backofficePage.getByPlaceholder('Numéro de téléphone').fill(world.phone);
+    
+    await world.backofficePage.locator('button[title="Confirmer"]').click()
+
+    // Mode de prise du rendez-vous
+    //await this.backofficePage.locator('#radio-taken-mode label').first().click(); // prise sur site
+    // Choix du Service (1st item)
+    const selectorPriseRdv = world.backofficePage.locator('span').filter({ hasText: 'Choisir un mode de prise de RDV' })
+    await selectorPriseRdv.click();
+    const selectorPriseRdvLegend = world.backofficePage.locator('span').filter({ hasText: 'Le rendez-vous a été pris :' })
+    const firstItemPriseRdv = selectorPriseRdvLegend.locator('..').locator('xpath=following-sibling::*').locator('div.multiselect__content-wrapper > ul.multiselect__content > li:first-child') 
+    await firstItemPriseRdv.click();
+
+    // ------------------------- Récuperation de la date/heure du rendez-vous initial -----------//
+    const dateInputLocator = world.backofficePage.getByText('Date du RDV').locator('xpath=following-sibling::*').locator('input[aria-label="Cliquez ici pour choisir la date"]');
+    world.initialAppointmentDate  = await dateInputLocator.inputValue();
+
+    // const selectorHeureDuRDV = world.backofficePage.locator('legend').filter({ hasText: 'Heure du RDV' }).locator('xpath=following-sibling::div//select');
+    world.initialAppointmentTime  = await selectorHeureDuRDV.inputValue();
+
+    // -------------------------  Confirmation du rendez-vous ----------------------- //
+    // Bloquer le créneau
+    await world.backofficePage.locator('button[title="Bloquer ce créneau"]').click()
+
+    // Confirmer le rendez-vous
+    const textLocator = world.backofficePage.getByText('Êtes-vous sur de vouloir ajouter cette réservation ?');
+    await textLocator.locator('xpath=following-sibling::*').locator(' button').filter({ hasText: 'Ajouter' }).click();
+
+    // Changer selecteur de nombre de jours a afficher
+    const filterLocator = world.backofficePage.locator('.calendar-mode-select .multiselect').nth(1);
+    await filterLocator.click();
+    await filterLocator.locator('.multiselect__content-wrapper ul li').filter({ hasText: "Journée", exact: true }).nth(0).click();
+
+    await world.backofficePage.waitForTimeout(1000);
+
+    var fullname = world.name.toUpperCase() + ' ' + world.firstname
+    const appointmentLocator = world.backofficePage.locator('span.font-weight-bold').filter({ hasText: fullname }).locator('xpath=..//..//..').nth(0);
+    await appointmentLocator.waitFor({ state: 'visible' });
+
+    world.oldDateRdv     = world.initialAppointmentDate
+    world.oldHeureRdv    = world.initialAppointmentTime
+    console.log('Initial date RDV: ' + world.initialAppointmentDate);
+    console.log('Initial heure RDV: ' + world.initialAppointmentTime);
+    console.log('old date RDV: ' + world.oldDateRdv);
+    console.log('old heure RDV: ' + world.oldHeureRdv);
+
+
+    // Récuperer la date courant
+    const now = new Date();
+    // Format the current date and time in 'DD/MM/YYYY HH:mm' format
+    world.appointmentDate    = String(now.getDate()).padStart(2, '0') + '/' + String(now.getMonth() + 1).padStart(2, '0') + '/' + now.getFullYear();
+    world.appointmentTime    = String(now.getHours()).padStart(2, '0') + ':' + String(now.getMinutes()).padStart(2, '0');
+    world.userName           = await world.backofficePage.locator('img[alt="Photo de profil"]').locator('xpath=preceding-sibling::span').textContent();
+
+    console.log('==> RDV CREATED');
+    
+    return { name: world.name, firstname: world.firstname, NIR: world.NIR, phone: world.phone, oldDateRdv: world.oldDateRdv, oldHeureRdv: world.oldHeureRdv, appointmentDate: world.appointmentDate, appointmentTime: world.appointmentTime, motif: world.motif};
+};
+
+
+Given("Un rendez-vous a été créer", async function() {
+        
+    //****************** Se connecter ******************/ 
+    this.loginPageAlt = new LoginPage(this.backofficePage);
+    await this.loginPageAlt.navigate();
+    await this.loginPageAlt.login(config.troovCafUserBackofficeUsername, config.troovCafUserBackofficePassword);
+
+    // wait for backoffice loaded
+    await this.backofficePage.waitForSelector('#page-topbar', { state: 'visible' }); 
+
+    let currentURL = await this.backofficePage.url();
+    while (!currentURL.includes('calendar')) {
+        await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+        currentURL = await this.backofficePage.url();
+    }
+    expect(await this.backofficePage.url()).toContain('calendar');
+
+    // Changer de compte en CNAF Formation
+    await this.backofficePage.locator('img.header-profile-user').click();
+    await this.backofficePage.locator('button[title="Changer de compte"]').click();
+    await this.backofficePage.waitForTimeout(3000); 
+
+    await this.backofficePage.locator('li[aria-label="CNAF Formation"] > div.p-tree-node-content > span.p-tree-node-label').click();
+
+    currentURL = await this.backofficePage.url();
+    while (!currentURL.includes('calendar')) {
+        await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+        currentURL = await this.backofficePage.url();
+    }
+    expect(await this.backofficePage.url()).toContain('calendar');
+
+
+    //****************** Création d'un rendez-vous ******************/
+    this.backofficePage.waitForTimeout(2000); 
+    const { name, firstname, NIR, phone, oldDateRdv, oldHeureRdv, appointmentDate, appointmentTime, motif } = await setupRdv(this);
+    this.name           = name;
+    this.firstname      = firstname;
+    this.NIR            = NIR;
+    this.phone          = phone;
+    this.dateRdv        = oldDateRdv;
+    this.heureRdv       = oldHeureRdv;
+    this.datePriseRdv  = appointmentDate;
+    this.heurePriseRdv  = appointmentTime;
+    this.motif          = motif;
+
+    console.log('oldDateRdv_1 ' + oldDateRdv);
+    console.log('oldHeureRdv_1 ' + oldHeureRdv);
+
+    console.log('this.dateRdv ' + this.dateRdv);
+    console.log('this.heureRdv ' + this.heureRdv);
+    console.log('==> Rendez-vous 1 créé pour ' + this.name + " " + this.firstname); 
+});
+
 
 Given("La page de confirmation de la création de ticket avec rendez-vous est ouverte", async function() {
 
@@ -19,10 +193,10 @@ Given("La page de confirmation de la création de ticket avec rendez-vous est ou
   const heading = await this.terminalPage.getByText("Je m'enregistre");
   await expect(heading).toBeVisible();
   const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
-  await inputNIRLocator.fill(global.NIR);
+  await inputNIRLocator.fill(this.NIR);
   const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
-  await inputPhoneLocator.fill(global.phone);
-  const buttonLocator = await this.terminalPage.locator('button[aria-label="Continuer"]');
+  await inputPhoneLocator.fill(this.phone);
+  const buttonLocator = await this.terminalPage.locator('button').filter({ hasText: 'Continuer' });
   await expect(buttonLocator).toBeEnabled();
   await buttonLocator.click();
 
@@ -46,6 +220,8 @@ Then("Tous les informations sur le signalement avec rendez-vous s'affiche correc
   // Vérifier que l'heure est au format "HHhMM" (e.g., "19h30")
   expect(rendezVousTime).toMatch(/^\d{2}h\d{2}$/);
 
+  expect(rendezVousTime.replace("h", ":")).toBe(this.heureRdv);
+
 	//***** */ Date/Heure de l'enregistrement
 	const dateTextLocator = this.terminalPage.getByText('Enregistré le');
   const dateEnregistrementLocator = dateTextLocator.locator('b:nth-of-type(1)');
@@ -60,11 +236,25 @@ Then("Tous les informations sur le signalement avec rendez-vous s'affiche correc
   // Vérifier que l'heure est au format "HHhMM"
   expect(recordedTime).toMatch(/^\d{2}h\d{2}$/);
 
+  console.log('recordedDate: ' + recordedDate);
+  console.log('recordedTime ' + recordedTime);
+  console.log('this.dateRdv ' + this.dateRdv);
+  console.log('this.heureRdv ' + this.heureRdv);
+
+  expect(recordedDate).toBe(this.datePriseRdv);
+  expect(recordedTime.replace("h", ":")).toBe(this.heurePriseRdv);
+
 	 //***** */ Motif
 		const motifTextLocator = this.terminalPage.getByText('Motif :');
 		const motifLocator = motifTextLocator.locator('b');
 		const motif = await motifLocator.textContent();
 		expect(motif).toBeTruthy();
+    expect(motif).toBe(this.motif);
+    
+    console.log('motif: ' + motif);
+    console.log(dateRdv);
+    console.log(heureRdv);
+    console.log(this.motif);
 });
 
 // Téléchargement du ticket digital
@@ -80,4 +270,31 @@ Then("le fichier ticket digital sur page de confirmation avec rendez-vous doit �
 	const filePath = await download.path();
 	expect(filePath).toBeTruthy();
 	await download.delete();
+});
+
+After(async function () {
+    console.log('==> CLEANING OF RDV');
+
+    if (!this.name || !this.firstname) {
+        console.log('==> Aucun rendez-vous a néttoyer');
+        return;
+    }
+
+    //***********************   Selectionne le rendez-vous *************************/
+    var fullname = this.name.toUpperCase() + ' ' + this.firstname
+    const appointmentLocator = this.backofficePage.locator('span.font-weight-bold').filter({ hasText: fullname }).locator('xpath=..//..//..').nth(0);
+    await appointmentLocator.click();
+    await this.backofficePage.waitForTimeout(1000);
+
+    //***********************   Clique sur annuler, puis Annuler ce/ces rendez-vous *************************/
+    const annulerBtnLocator = this.backofficePage.locator('#reservation-edit-modal button').filter({ hasText: "Annuler ce/ces rendez-vous" });
+    
+    await annulerBtnLocator.click();
+    const annulerCesRdvBtnLocator = this.backofficePage.locator('#send-message span:has-text("Annuler ce/ces rendez-vous")').locator('..');
+    await annulerCesRdvBtnLocator.click();
+
+
+    await appointmentLocator.waitFor({ state: 'detached', timeout: 5000 });
+
+    console.log('==> RDV deleted');
 });
