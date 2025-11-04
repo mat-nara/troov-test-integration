9e91b4702de3ce05ca2c98b123166624618ce100
Mahefa <mahefa@troov.com>
2025-11-04 14:38:49 +0300

    Auto suppression des tickets créer

--- src/test/signalement-usager/steps/selection-sans-rdv-confirmation.step.js
+++ src/test/signalement-usager/steps/selection-sans-rdv-confirmation.step.js
@@ -1,9 +1,24 @@
 const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
+const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
-const config = require('../../../../config/env.js')
+const config = require('../../../../config/env.js');
+const { generateRandomNIR, generateRandomPhone } = require('../../signalement-usager/utils/helper');
+const LoginPage = require('../../signalement-usager/pages/LoginPage');
+
 
 
 setDefaultTimeout(60 * 1000);
+
+function randomNIR() {
+  const sexe = Math.random() < 0.5 ? 1 : 2;          // 1 ou 2
+  const annee = String(Math.floor(Math.random() * 100)).padStart(2, '0'); // 00..99
+  const mois = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0'); // 01..12
+  const dept = String(Math.floor(Math.random() * 96) + 1).padStart(2, '0'); // 01..95
+  const commune = String(Math.floor(Math.random() * 990) + 1).padStart(3, '0');
+  const ordre = String(Math.floor(Math.random() * 990) + 1).padStart(3, '0');
+
+  return `${sexe} ${annee} ${mois} ${dept} ${commune} ${ordre}`;
+}
 
 Given("La page de confirmation de la création de ticket sans rendez-vous est ouverte", async function() {
 
@@ -17,9 +32,9 @@ Given("La page de confirmation de la création de ticket sans rendez-vous est ou
   const heading = await this.terminalPage.getByText("Je m'enregistre");
   await expect(heading).toBeVisible();
   const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
-  await inputNIRLocator.fill('1234567891111');
+  await inputNIRLocator.fill(randomNIR());
   const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
-  await inputPhoneLocator.fill('1234567891');
+  await inputPhoneLocator.fill(generateRandomPhone());
   const buttonLocator = await this.terminalPage.locator('button').filter({ hasText: 'Continuer' });
   await expect(buttonLocator).toBeEnabled();
   await buttonLocator.click();
@@ -86,5 +101,66 @@ Then("Le fichier ticket digital sur page de confirmation sans rendez-vous doit �
 	const filePath = await download.path();
 	expect(filePath).toBeTruthy();
 	await download.delete();
+});
+
+After(async function () {
+    console.log('==> CLEANING OF RDV');
+    
+    // Chercher le numero du ticket
+    const pElement = this.terminalPage.locator('text="Vous êtes bien enregistré !"').locator('xpath=../following-sibling::*[1]/child::*[2]/p');
+    this.ticket = await pElement.textContent();
+
+    if (!this.ticket) {
+        console.log('==> Aucun ticket a néttoyer');
+        return;
+    }
+
+    //***********************   Login Backoffice *************************/
+    this.loginPageAlt = new LoginPage(this.backofficePage);
+    await this.loginPageAlt.navigate(this.backofficePage);
+    await this.loginPageAlt.login(config.troovCafUserBackofficeUsername, config.troovCafUserBackofficePassword);
+
+    // wait for backoffice loaded
+    await this.backofficePage.waitForSelector('#page-topbar', { state: 'visible' }); 
+
+    let currentURL = await this.backofficePage.url();
+    while (!currentURL.includes('calendar')) {
+        await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+        currentURL = await this.backofficePage.url();
+    }
+    expect(await this.backofficePage.url()).toContain('calendar');
+
+    // Changer de compte en CNAF Formation
+    await this.backofficePage.locator('img.header-profile-user').click();
+    await this.backofficePage.locator('button[title="Changer de compte"]').click();
+    await this.backofficePage.waitForTimeout(3000); 
+
+    await this.backofficePage.locator('li[aria-label="CNAF Formation"] > div.p-tree-node-content > span.p-tree-node-label').click();
+
+    currentURL = await this.backofficePage.url();
+    while (!currentURL.includes('calendar')) {
+        await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+        currentURL = await this.backofficePage.url();
+    }
+    expect(await this.backofficePage.url()).toContain('calendar');
+
+
+    //***********************   Supprimer le ticket créé *************************/
+    
+    await this.backofficePage.locator('i[title="File d\'attente"]').click();
+    await this.backofficePage.waitForTimeout(2000); 
+
+    // Localiser le ticket dans la section "Attente sans RDV"
+    const ticketBlock = this.backofficePage.getByText(/Attente sans RDV \(\d+\)/)
+                                           .locator('xpath=..//..//..')
+                                           .locator('div.font-size-large.w-25:has-text("' + this.ticket + '")');
+    
+    await ticketBlock.locator('..').locator('button[title="Annuler le ticket"]').click();
+    await this.backofficePage.waitForTimeout(1000);
+    await this.backofficePage.locator('.modal-dialog footer button').filter({ hasText: 'Oui' }).click();
+
+    await ticketBlock.waitFor({ state: 'detached', timeout: 5000 });
+
+    console.log('==> RDV deleted');
 });
 
