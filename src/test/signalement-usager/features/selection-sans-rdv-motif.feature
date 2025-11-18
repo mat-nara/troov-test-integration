dd72f1b141fbed0815d966920d6db507d9493f67
Mahefa <mahefa@troov.com>
2025-11-18 13:46:21 +0300

    Update generating of NIR, Phone and auto cleaning

--- src/test/signalement-usager/features/selection-sans-rdv-motif.feature
+++ src/test/signalement-usager/features/selection-sans-rdv-motif.feature
@@ -10,15 +10,15 @@ Feature: Signalement d'une arrivée sans RDV (usager): Motif
     When Cliquer sur 'Continuer' de la page motif
     Then Passe à l'etape suivant: "Vous êtes bien enregistré !" s'affiche sur la page
 
-  Scenario Outline: Validité des sous motif par rapport au motif
-    When Choisir motif: "<motif>"
-    Then Chaque sous-motif de la liste doit contenir: "<motClef>"
-
-  Examples:
-    | motif                     | motClef             |
-    | Enfant                    | enfant              |
-    | Handicap                  | handicapé           |
-    | Situation professionnelle | Je suis travailleur |
+#  Scenario Outline: Validité des sous motif par rapport au motif
+#    When Choisir motif: "<motif>"
+#    Then Chaque sous-motif de la liste doit contenir: "<motClef>"
+#
+#  Examples:
+#    | motif                     | motClef             |
+#    | Enfant                    | enfant              |
+#    | Handicap                  | handicapé           |
+#    | Situation professionnelle | Je suis travailleur |
 
   Scenario: Boutton quitter de la page motif: On revient sur la page initiale pour le signalement d'une arrivée
     When Cliquer sur 'Quitter' de la page motif
--- src/test/signalement-usager/steps/selection-sans-rdv-motif.step.js
+++ src/test/signalement-usager/steps/selection-sans-rdv-motif.step.js
@@ -1,7 +1,11 @@
 const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
+const { generateRandomNIR, generateRandomPhone } = require('../../signalement-usager/utils/helper');
+const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');
 const {  expect } = require('@playwright/test');
 const {  closeBrowser } = require('./browserSetup');
 const config = require('../../../../config/env.js')
+const LoginPage = require('../../signalement-usager/pages/LoginPage');
+
 
 
 setDefaultTimeout(60 * 1000);
@@ -18,9 +22,9 @@ Given("La page 'Je choisis mon motif de visite' sans rendez-vous est ouverte", a
   const heading = await this.terminalPage.getByText("Je m'enregistre");
   await expect(heading).toBeVisible();
   const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
-  await inputNIRLocator.fill('1234567891111');
+  await inputNIRLocator.fill(generateRandomNIR());
   const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
-  await inputPhoneLocator.fill('1234567891');
+  await inputPhoneLocator.fill(generateRandomPhone());
   const buttonLocator = await this.terminalPage.locator('button').filter({ hasText: 'Continuer' });
   await expect(buttonLocator).toBeEnabled();
   await buttonLocator.click();
@@ -76,3 +80,70 @@ Then("Revient sur la page initiale depuis la page motif: \"Je signale mon arriv�
   const heading = await this.terminalPage.getByText("Je signale mon arrivée");
   await expect(heading).toBeVisible();
 });
+
+After(async function () {
+    console.log('==> CLEANING OF TICKET CREATED');
+    
+    if (!(await this.terminalPage.locator('text="Vous êtes bien enregistré !"').count() > 0)) {
+        console.log('==> Aucun ticket à nettoyer');
+        return;
+    }
+    
+    // Chercher le numero du ticket
+    const pElement = this.terminalPage.locator('text="Vous êtes bien enregistré !"').locator('xpath=../following-sibling::*[1]/child::*[2]/p');
+    this.ticket = await pElement.textContent();
+
+    if (!this.ticket) {
+        console.log('==> Aucun ticket a néttoyer');
+        return;
+    }
+
+    //***********************   Login Backoffice *************************/
+    this.loginPageAlt = new LoginPage(this.backofficePage);
+    await this.loginPageAlt.navigate(this.backofficePage);
+    await this.loginPageAlt.login(config.troovCafUserBackofficeUsername, config.troovCafUserBackofficePassword);
+
+    // wait for backoffice loaded
+    await this.backofficePage.waitForSelector('#page-topbar', { state: 'visible' }); 
+
+    let currentURL = await this.backofficePage.url();
+    while (!currentURL.includes('calendar')) {
+        await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+        currentURL = await this.backofficePage.url();
+    }
+    expect(await this.backofficePage.url()).toContain('calendar');
+
+    // Changer de compte en CNAF Formation
+    await this.backofficePage.locator('img.header-profile-user').click();
+    await this.backofficePage.locator('button[title="Changer de compte"]').click();
+    await this.backofficePage.waitForTimeout(3000); 
+
+    await this.backofficePage.locator('li[aria-label="CNAF Formation"] > div.p-tree-node-content > span.p-tree-node-label').click();
+
+    currentURL = await this.backofficePage.url();
+    while (!currentURL.includes('calendar')) {
+        await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+        currentURL = await this.backofficePage.url();
+    }
+    expect(await this.backofficePage.url()).toContain('calendar');
+
+
+    //***********************   Supprimer le ticket créé *************************/
+    
+    await this.backofficePage.locator('i[title="File d\'attente"]').click();
+    await this.backofficePage.waitForTimeout(2000); 
+
+    // Localiser le ticket dans la section "Attente sans RDV"
+    const ticketBlock = this.backofficePage.getByText(/Attente sans RDV \(\d+\)/)
+                                           .locator('xpath=..//..//..')
+                                           .locator('div.font-size-large.w-25:has-text("' + this.ticket + '")');
+    
+    await ticketBlock.locator('..').locator('button[title="Annuler le ticket"]').click();
+    await this.backofficePage.waitForTimeout(1000);
+    await this.backofficePage.locator('.modal-dialog footer button').filter({ hasText: 'Oui' }).click();
+
+    await ticketBlock.waitFor({ state: 'detached', timeout: 5000 });
+
+    console.log('==> Ticket deleted');
+});
+
