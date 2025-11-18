d2c6c0055cdd577b216760027ee4e2b07877fbc6
Mahefa <mahefa@troov.com>
2025-11-18 15:15:58 +0300

    Update generator NIR and Phone, update auto cleaning and email generation

--- src/test/signalement-usager/steps/selection-avec-rdv-recherche.step.js
+++ src/test/signalement-usager/steps/selection-avec-rdv-recherche.step.js
@@ -2,25 +2,13 @@ const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');
 const config = require('../../../../config/env.js')
-const { generateRandomNIR, generateRandomPhone } = require('../utils/helper');
+const { generateRandomNIR, generateRandomPhone } = require('../../signalement-usager/utils/helper');
+
 const LoginPage = require('../pages/LoginPage');
 const { faker, fakerFR  } = require('@faker-js/faker');
 
 
-
-
 setDefaultTimeout(60 * 1000);
-
-function randomNIR() {
-  const sexe = Math.random() < 0.5 ? 1 : 2;          // 1 ou 2
-  const annee = String(Math.floor(Math.random() * 100)).padStart(2, '0'); // 00..99
-  const mois = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0'); // 01..12
-  const dept = String(Math.floor(Math.random() * 96) + 1).padStart(2, '0'); // 01..95
-  const commune = String(Math.floor(Math.random() * 990) + 1).padStart(3, '0');
-  const ordre = String(Math.floor(Math.random() * 990) + 1).padStart(3, '0');
-
-  return `${sexe} ${annee} ${mois} ${dept} ${commune} ${ordre}`;
-}
 
 
 async function setupRdv(world) {
@@ -59,9 +47,13 @@ async function setupRdv(world) {
     // Generate random data for appointment
     world.name = faker.person.lastName();
     world.firstname = faker.person.firstName();
-    world.email = world.name.toLowerCase() + '@test.com';
-    // world.NIR = generateRandomNIR();
-    world.NIR = randomNIR()
+    // world.email = world.name.toLowerCase() + '@test.com';
+    // this.email = this.name.toLowerCase() + '@test.com';
+
+    const randomNumber = Math.floor(100 + Math.random() * 999); // Générer un nombre aléatoire à 6 chiffres
+    world.email = world.name.toLowerCase() + "-" + world.firstname.toLowerCase() + "-" + randomNumber.toString() + '@test.com';
+
+    world.NIR = generateRandomNIR();
     world.phone = generateRandomPhone();
 
     await world.backofficePage.getByPlaceholder('Ajouter un Nom').fill(world.name);
@@ -205,7 +197,7 @@ Given("que NIR et Téléphone sont saisie", async function() {
 
 // NIR sans RDV: Un message d'erreur s'affiche
 Given("que NIR sans RDV est saisie", async function() {
-    var NIR = randomNIR();
+    var NIR = generateRandomNIR();
     const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
     await inputNIRLocator.fill(NIR);
 
@@ -227,25 +219,26 @@ Given("que phone sans RDV est saisie", async function() {
 Given("NIR saisie au mauvais format", async function() {
     const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
 
-    this.NIR = randomNIR();
-    this.phone = generateRandomPhone();
+    this.NIR    = generateRandomNIR();
+    this.phone  = generateRandomPhone();
 
     // remove one character from NIR
     var NIR = this.NIR
     NIR = NIR.substring(0, NIR.length - 1)
     await inputNIRLocator.fill(NIR); // 12 caracter au lieu de 13
+    await this.terminalPage.mouse.click(10, 10);
   
     const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
     await inputPhoneLocator.fill(this.phone);
   
-    const buttonLocator = await this.terminalPage.locator('button').filter({ hasText: 'Continuer' });
-    await expect(buttonLocator).toBeEnabled();
+    // const buttonLocator = await this.terminalPage.locator('button').filter({ hasText: 'Continuer' });
+    // await expect(buttonLocator).toBeEnabled();
 });
 
 // Téléphone dans un mauvais format: Un message d'erreur s'affiche
 Given("Téléphone saisie au mauvais format", async function() {
-    this.NIR = randomNIR();
-    this.phone = generateRandomPhone();
+    this.NIR    = generateRandomNIR();
+    this.phone  = generateRandomPhone();
 
     const inputNIRLocator = this.terminalPage.locator('label[for="social-security-number"] + input');
     await inputNIRLocator.fill(this.NIR); 
@@ -256,9 +249,10 @@ Given("Téléphone saisie au mauvais format", async function() {
   
     const inputPhoneLocator = this.terminalPage.locator('label[for="phone-number"] + input');
     await inputPhoneLocator.fill(phone); // 9 caracter au lieu de 10
+    await this.terminalPage.mouse.click(10, 10);
   
-    const buttonLocator = await this.terminalPage.locator('button').filter({ hasText: 'Continuer' });
-    await expect(buttonLocator).toBeEnabled();
+    // const buttonLocator = await this.terminalPage.locator('button').filter({ hasText: 'Continuer' });
+    // await expect(buttonLocator).toBeEnabled();
   });
 
 
@@ -294,16 +288,12 @@ Then("Revient sur la page initiale: \"Je signale mon arrivée\" s'affiche sur la
     await expect(heading).toBeVisible();
 });
 
-
-
-Then("Message d'erreur NIR incorrecte s'affiche", async function() {
-    const heading = await this.terminalPage.getByText("Saisie incorrecte (13 caractères ; Exemple : 2 94 03 75 120 005)");
-    await expect(heading).toBeVisible();
+Then("Message d'erreur associé au NIR s'affiche", async function() {
+  await expect(this.terminalPage.locator('div').filter({ hasText: "Format invalide : Le numéro de Sécurité sociale n'est pas valide (exemple : 2 94 03 75 120 005)" }).first()).toBeVisible();
 });
 
-Then("Message d'erreur phone incorrecte s'affiche", async function() {
-    const heading = await this.terminalPage.getByText("Saisie incorrecte (10 caractères ; Exemple : 07 21 30 89 28)");
-    await expect(heading).toBeVisible();
+Then("Message d'erreur associé au Téléphone s'affiche", async function() {
+  await expect(this.terminalPage.locator('div').filter({ hasText: "Saisie incorrecte (10 caractères ; Exemple : 07 21 30 89 28)" }).first()).toBeVisible();
 });
 
 Then("Message d'erreur 'Aucun rendez-vous' s'affiche", async function() {
@@ -337,4 +327,39 @@ After(async function () {
     await appointmentLocator.waitFor({ state: 'detached', timeout: 5000 });
 
     console.log('==> RDV deleted');
+
+    //*********************************************** End of suppression du rendez-vous *************************/
+    console.log('==> CLEANING OF TICKET CREATED');
+    
+    if (!(await this.terminalPage.locator('text="Vous êtes bien enregistré !"').count() > 0)) {
+        console.log('==> Aucun ticket à nettoyer');
+        return;
+    }
+    
+    // Chercher le numero du ticket
+    const pElement = this.terminalPage.locator('text="Vous êtes bien enregistré !"').locator('xpath=../following-sibling::*[1]/child::*[2]/p');
+    this.ticket = await pElement.textContent();
+
+    if (!this.ticket) {
+        console.log('==> Aucun ticket a néttoyer');
+        return;
+    }
+    
+    //***********************   Supprimer le ticket créé *************************/
+    
+    await this.backofficePage.locator('i[title="File d\'attente"]').click();
+    await this.backofficePage.waitForTimeout(2000); 
+
+    // Localiser le ticket dans la section "Attente sans RDV"
+    const ticketBlock = this.backofficePage.getByText(/Attente avec RDV \(\d+\)/)
+                                           .locator('xpath=..//..//..')
+                                           .locator('div.font-size-large.w-25:has-text("' + this.ticket + '")');
+    
+    await ticketBlock.locator('..').locator('button[title="Annuler le ticket"]').click();
+    await this.backofficePage.waitForTimeout(1000);
+    await this.backofficePage.locator('.modal-dialog footer button').filter({ hasText: 'Oui' }).click();
+
+    await ticketBlock.waitFor({ state: 'detached', timeout: 5000 });
+
+    console.log('==> Ticket deleted');
 });
