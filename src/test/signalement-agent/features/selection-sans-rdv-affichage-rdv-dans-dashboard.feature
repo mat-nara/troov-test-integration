117d7363610d12ab4d68a03c221ac7c36fd9c7f3
Mahefa <mahefa@troov.com>
2025-12-04 15:14:01 +0300

    update new process, add autocleanning

--- src/test/signalement-agent/features/selection-sans-rdv-affichage-rdv-dans-dashboard.feature
+++ src/test/signalement-agent/features/selection-sans-rdv-affichage-rdv-dans-dashboard.feature
@@ -1,9 +1,11 @@
-@authenticated @SignalementAgentSansRdvAffichageRdvDansDashboard  
+@SignalementAgentSansRdvAffichageRdvDansDashboard  
 Feature: Signalement d'une arrivée sans RDV (agent): Vérification que le signalement d'une arrivée s'affiche correctement dans la file d'attente. 
 
   Background:
-        Given qu’un signalement d’arrivée côté agent sans rendez-vous est confirmé.
+    Given L'utilisateur est connecté à l'application Troov
+        * que un signalement d'arrivée sans rendez-vous est confirmé 
 
-  Scenario: Vérification que le signalement d'une arrivé s'affiche dans la file d'attente du lieu concerné et vérification de l'exactitude des informations du ticket confirmé
-    When la page de la file d'attente du backoffice est ouverte | signalement sans rendez-vous côté agent
-    Then Le signalement sans rendez-vous côté agent doit s'afficher dans la file d'attente avec les informations correspondantes
+  Scenario: Vérification le signalement arrivé s'affiche dans la file d'attente du lieu concerné  
+    When la page de la file d'attente du backoffice est ouverte
+    Then le ticket doit s'afficher dans la file d'attente sans rendez-vous
+       * Le numéro et le motif du ticket confirmé dans le signalement sans rendez-vous doivent être identiques à ceux présents dans la file d'attente.
--- src/test/signalement-agent/steps/selection-sans-rdv-affichage-rdv-dans-dashboard.step.js
+++ src/test/signalement-agent/steps/selection-sans-rdv-affichage-rdv-dans-dashboard.step.js
@@ -1,86 +1,164 @@
 const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
-const config = require('../../../../config/env.js');
-const exp = require('constants');
-const { fakerFR } = require('@faker-js/faker');
+const config = require('../../../../config/env.js')
+const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');
+const { generateRandomNIR, generateRandomPhone } = require('../../signalement-usager/utils/helper');
+const LoginPage = require('../../signalement-usager/pages/LoginPage');
+
+
 
 setDefaultTimeout(60 * 1000);
 
-let motif, nom, prenom;
+let ticket = ""
+let motif = ""
 
-Given("qu’un signalement d’arrivée côté agent sans rendez-vous est confirmé.", async function() {
+
+Given("L'utilisateur est connecté à l'application Troov", async function() {
+  
+	//***********************   Login Backoffice *************************/
+	this.loginPageAlt = new LoginPage(this.backofficePage);
+	await this.loginPageAlt.navigate(this.backofficePage);
+	await this.loginPageAlt.login(config.troovCafUserBackofficeUsername, config.troovCafUserBackofficePassword);
+
+	// wait for backoffice loaded
+	await this.backofficePage.waitForSelector('#page-topbar', { state: 'visible' }); 
+
+	let currentURL = await this.backofficePage.url();
+	while (!currentURL.includes('calendar')) {
+		await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+		currentURL = await this.backofficePage.url();
+	}
+	expect(await this.backofficePage.url()).toContain('calendar');
+
+	// Changer de compte en CNAF Formation
+	await this.backofficePage.locator('img.header-profile-user').click();
+	await this.backofficePage.locator('button[title="Changer de compte"]').click();
+	await this.backofficePage.waitForTimeout(3000); 
+
+	await this.backofficePage.locator('li[aria-label="CNAF Formation"] > div.p-tree-node-content > span.p-tree-node-label').click();
+
+	currentURL = await this.backofficePage.url();
+	while (!currentURL.includes('calendar')) {
+		await this.backofficePage.waitForTimeout(1000); // wait for 1 second before checking again
+		currentURL = await this.backofficePage.url();
+	}
+	expect(await this.backofficePage.url()).toContain('calendar');
+});
+
+
+Given("que un signalement d'arrivée sans rendez-vous est confirmé", async function() {
+
 	await this.backofficePage.locator('i[title="File d\'attente"]').click();
 	const signalerArriveeButton = await this.backofficePage.locator('button[title="Signaler une arrivée"]');
 	await signalerArriveeButton.waitFor({ state: 'visible' });
 	await signalerArriveeButton.click();
-	await this.backofficePage.waitForTimeout(2000);
+	await this.backofficePage.locator('button').filter({ hasText: "Usager sans rendez-vous" }).click();
+	await expect(this.backofficePage.getByText("Enregistrer l’usager")).toBeVisible();
 
-	// Page principale signalement d'un arrivée
-	const headingSignalement = await this.backofficePage.getByText("Je signale une arrivée pour :");
-	await expect(headingSignalement).toBeVisible();
+	// Remplir les informations de l'usager
+	const inputNIRLocator = this.backofficePage.locator('label[for="social-security-number"] + input');
+	await inputNIRLocator.fill(generateRandomNIR());
+	const inputPhoneLocator = this.backofficePage.locator('label[for="phone-number"] + input');
+	await inputPhoneLocator.fill(generateRandomPhone());
+	const buttonLocator = await this.backofficePage.locator('button').filter({ hasText: 'Continuer' });
+	await expect(buttonLocator).toBeEnabled();
+	await this.backofficePage.locator('button').filter({ hasText: 'Continuer' }).click();
 
-	const sansRdvButton = this.backofficePage.locator('button:has-text("Un usager sans RDV")');
-	await sansRdvButton.click();
-	await this.backofficePage.waitForTimeout(2000);
+	// Page Motif de visite 
+	await expect(await this.backofficePage.getByText("Choisir le motif de visite")).toBeVisible();
+	const inputOfficeTypeLocator = this.backofficePage.locator('label[for="office-type"] + div span.p-select-label');
+	await inputOfficeTypeLocator.click();
+	await this.backofficePage.locator(`li[aria-label="Enfant"]`).click();
+	const updatedTextOfficeType = await inputOfficeTypeLocator.textContent();
+	expect(updatedTextOfficeType.trim()).toBe("Enfant");
+	await this.backofficePage.locator('label[for="office"] + div span.p-select-label').click();
+	const liAttendEnfantLocator = await this.backofficePage.locator(`li[aria-label="J'attends / J'accueille un enfant"]`);
+	//const liAttendEnfantLocator = await this.backofficePage.locator(`li[aria-label="J’ai besoin d’aide pour mes démarches en ligne"]`);
+	await liAttendEnfantLocator.click();
+	const updatedTextOffice = await liAttendEnfantLocator.textContent();
+	expect(updatedTextOffice.trim()).toBe("J'attends / J'accueille un enfant");
+	await this.backofficePage.locator('button').filter({ hasText: 'Continuer' }).click();
 
-	// Choisir un service
-	const headingService = await this.backofficePage.getByText("Je choisis le service pour lequel l’usager souhaite prendre un RDV");
-	await expect(headingService).toBeVisible();
+  	// Page de confirmation
+  	await expect(await this.backofficePage.getByText("L’usager a été ajouté dans la file d’attente")).toBeVisible();
 
-	const nextSibling = headingService.locator('xpath=following-sibling::*[1]');
-	const firstServiceButton = nextSibling.locator('xpath=child::*[1]').locator('button');
-	await firstServiceButton.click();
-
-	motif = await firstServiceButton.textContent();
-
-	const validerButton = this.backofficePage.locator('button:has-text("Valider")');
-	await validerButton.click();
-
-	// Page Information de l'usager
-	const heading = await this.backofficePage.getByText("Je renseigne les informations de l’usager qui souhaite prendre un RDV");
-	await expect(heading).toBeVisible();  
-
-	nom = fakerFR.person.lastName();
-	await this.backofficePage.locator('#lastname').fill(nom);
-	prenom = fakerFR.person.firstName();
-	await this.backofficePage.locator('#firstname').fill(prenom);
-	await this.backofficePage.waitForTimeout(1000);
-	const validerInfoUserButton = this.backofficePage.locator('button:has-text("Valider")');
-	await validerInfoUserButton.click();
-	await this.backofficePage.waitForTimeout(1000);
-
-	// Page confirmation
-	await this.backofficePage.getByText("Je confirme sans imprimer le ticket").click();
-	await this.backofficePage.waitForTimeout(5000);
+	// Chercher le numero du ticket
+	ticket = await this.backofficePage.locator('text="Numéro à communiquer à l’usager"').locator('xpath=/following-sibling::div[1]/p').textContent();
 	
+	// Cherche le motif du ticket
+	const motifInfo = await this.backofficePage.getByText('Motif :').textContent();
+	const match = motifInfo.match(/Motif :\s*(.*)/);
+	motif = match[1].trim();
+	console.log('==> Ticket created: ' + ticket + ' with motif: ' + motif);
+
+	this.backofficePage.locator('button').filter({ hasText: 'Terminer' }).click()
 });
 
-When("la page de la file d'attente du backoffice est ouverte | signalement sans rendez-vous côté agent", async function() {
-	await this.backofficePage.locator('i[title="Mon équipe"]').click();
-  const headEquipe = await this.backofficePage.getByText("Liste de vos équipes");
-  await expect(headEquipe).toBeVisible();
-	
+When("la page de la file d'attente du backoffice est ouverte", async function() {
   await this.backofficePage.locator('i[title="File d\'attente"]').click();
-  const element = await this.backofficePage.getByText("Usagers en attente:");
-  await expect(element).toBeVisible();
-	
+  await expect(this.backofficePage.getByText("Usagers en attente:")).toBeVisible();
 });
 
-Then("Le signalement sans rendez-vous côté agent doit s'afficher dans la file d'attente avec les informations correspondantes", async function() {
-
+Then("le ticket doit s'afficher dans la file d'attente sans rendez-vous", async function() {
+  console.log('ticket number: ', ticket);
+  
   const attenteSansRDVHeader = this.backofficePage.getByText(/Attente sans RDV \(\d+\)/);
   const parentBlock = attenteSansRDVHeader.locator('xpath=..//..//..'); 
-  //const ticketBlock = parentBlock.locator('div.font-size-large.w-25:has-text("' + ticket + '")');
-	const prenomLocator = parentBlock.getByText('● ' + prenom);
+  const ticketBlock = parentBlock.locator('div.font-size-large.w-25:has-text("' + ticket + '")');
 
-	// Vérification prénom
-	await expect(prenomLocator).toBeVisible();
+  await expect(ticketBlock).toBeVisible();
+});
 
-	// Vérification nom et prénom
-	const nomLocator = prenomLocator.getByText(nom);
-	await expect(nomLocator).toBeVisible();
+Then("Le numéro et le motif du ticket confirmé dans le signalement sans rendez-vous doivent être identiques à ceux présents dans la file d'attente.", async function() {
+  const attenteSansRDVHeader = this.backofficePage.getByText(/Attente sans RDV \(\d+\)/);
+  const parentBlock = attenteSansRDVHeader.locator('xpath=..//..//..'); 
+  const ticketBlock = parentBlock.locator('div.font-size-large.w-25:has-text("' + ticket + '")');
 
-	// vérification motif
-	const motifLocator = prenomLocator.getByText(motif);
-	await expect(motifLocator).toBeVisible();
+  await expect(ticketBlock).toBeVisible();
+
+  const motifBlock = ticketBlock.locator('xpath=..//..').getByText(motif);
+  await expect(motifBlock).toBeVisible();
+
+  this.ticket = ticket; // Store ticket for After hook
+});
+
+
+After(async function () {
+    console.log('==> CLEANING OF TICKET CREATED');
+    
+//    if (!(await this.backofficePage.locator('text="Numéro à communiquer à l’usager"').count() > 0)) {
+//        console.log('==> Aucun ticket à nettoyer');
+//        return;
+//    }
+//    
+//    // Chercher le numero du ticket
+//    const pElement = this.backofficePage.locator('text="Numéro à communiquer à l’usager"').locator('xpath=/following-sibling::div[1]/p');
+//    this.ticket = await pElement.textContent();
+//	console.log('==> Ticket to delete: ' + this.ticket);
+//
+//    if (!this.ticket) {
+//        console.log('==> Aucun ticket a néttoyer');
+//        return;
+//    }
+	// await this.backofficePage.locator('button').filter({ hasText: 'Terminer' }).click()
+	console.log('==> Ticket to delete: ' + this.ticket);
+
+
+    //***********************   Supprimer le ticket créé *************************/
+    
+    await this.backofficePage.locator('i[title="File d\'attente"]').click();
+    await this.backofficePage.waitForTimeout(2000); 
+
+    // Localiser le ticket dans la section "Attente sans RDV"
+    const ticketBlock = this.backofficePage.getByText(/Attente sans RDV \(\d+\)/)
+                                           .locator('xpath=..//..//..')
+                                           .locator('div.font-size-large.w-25:has-text("' + this.ticket + '")');
+    
+    await ticketBlock.locator('..').locator('button[title="Annuler le ticket"]').click();
+    await this.backofficePage.waitForTimeout(1000);
+    await this.backofficePage.locator('.modal-dialog footer button').filter({ hasText: 'Oui' }).click();
+
+    await ticketBlock.waitFor({ state: 'detached', timeout: 5000 });
+
+    console.log('==> Ticket deleted');
 });
