3269bf0fab2560e6e5b758a240c81e7caeae6c34
Fanilo RAMAHENINTSOA <fanilo.info@socota.local>
2026-08-03 16:57:27 +0300

    creation noueau step sur la signalement d'objet avec IA et modification d'objet

--- cucumber-profiles.js
+++ cucumber-profiles.js
@@ -435,6 +435,12 @@ const testList = [
   profileName: "signalement-agent-modification-objet",
   feature: "src/troov-objets/signalement-agent/features/modification-objet.feature",
   step: "src/troov-objets/signalement-agent/steps/{modification-objet,connexion-agent}.step.js"
+},
+// signalement objet avec IA 
+  {
+  profileName: "signalement-agent-signalement-objet-avec-ia",
+  feature: "src/troov-objets/signalement-agent/features/signalement-objet-avec-ia.feature",
+  step: "src/troov-objets/signalement-agent/steps/{signalement-objet-avec-ia,connexion-agent}.step.js"
 }
 ];
 
